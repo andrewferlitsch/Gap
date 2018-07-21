15a3c6c07abcbb470ddc2116dbccaf3bc4b6a725
Andrew Ferlitsch <aferlitsch@gmail.com>
2018-07-21 06:48:15 -0700

    add missing setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Gap-ML',
+      version='0.9',
+      description='NLP and CV Data Engineering Framework',
+      author='Andrew Ferlitsch',
+      author_email='aferlitsch@gmail.com',
+      license='CC-BY',
+      url='',
+      packages=['./'],
+     )
