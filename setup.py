a254a05966a5eb26576d119038c8b846749d904f
David Molina <33104726+virtualdvid@users.noreply.github.com>
2018-07-23 18:42:52 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+import os, sys
 
 setup(name='Gap-ML',
       version='0.9',
@@ -9,12 +10,26 @@ setup(name='Gap-ML',
       author_email='aferlitsch@gmail.com',
       license='CC-BY',
       url='https://github.com/andrewferlitsch/Gap',
-      packages=['./'],
-       install_requires=[
+      packages=find_packages(),
+      install_requires=[
           'numpy',
           'h5py',
           'unidecode',
           'nltk',
-          'cv2'
+          'opencv-python'
       ],
      )
+
+if sys.platform.startswith('win'):
+    pass
+elif sys.platform.startswith('linux'):
+    #install Ghostscript
+    os.system('sudo apt-get update && sudo apt-get install ghostscript')
+    #install ImageMagick:
+    os.system('sudo apt-get install imagemagick')
+    #install Tesseract:
+    os.system('sudo apt-get install tesseract-ocr && sudo apt-get install tesseract-ocr-eng')
+elif sys.platform.startswith('darwin'):
+    os.system('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
+    os.system('brew update')
+    os.system('brew install ghostscript imagemagick tesseract')
