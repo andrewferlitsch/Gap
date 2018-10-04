40c631c015d1122d980ad5c8ae95dea5509f041b
David Molina <33104726+virtualdvid@users.noreply.github.com>
2018-10-04 11:01:23 -0700

    Fixed f' format,
    Added docstring to describe the module

--- tests/number_verify.py
+++ tests/number_verify.py
@@ -2,36 +2,40 @@
 Copyright, 2018(c), Andrew Ferlitsch
 Autor: David Molina @virtualdvid
 """
+
+"""
+number_verify: Checks each tests funtions names and re-enumerate if names are duplicated.
+"""
 import glob
 
 test_files = [f for f in glob.glob("*_test.py")]
 
 for file in test_files:
-    if True:
-        f = open(file, 'r')
-        func_list = [line[8:16] for line in f if line[8:13]=='test_']
-        func_unic = []
-        func_dupl = []
-        for func in func_list:
-            if func not in func_unic:
-                func_unic.append(func)
-            else:
-                func_dupl.append(func)
-
-        if not func_dupl:
-            print(f'Checked module {file} not duplicate functions were found')
+    f = open(file, 'r')
+    func_list = [line[8:16] for line in f if line[8:13]=='test_']
+    func_unic = []
+    func_dupl = []
+    for func in func_list:
+        if func not in func_unic:
+            func_unic.append(func)
         else:
-            f = open(file, 'r')
-            f_name=file.split('.')
-            cp = f_name[0]+'_cp.py'
-            f2 = open(cp,'w+')
-            i=1
-            for line in f:
-                if 'test_' in line and 'x' not in line:
-                    f2.write(line.replace(line[8:16],'test_'+str(i).zfill(3)))
-                    i+=1
-                else:
-                    f2.write(line)
-            print(f'Checked module {file} this functions were duplicates {func_dupl} and fixed on {cp}')
-            f2.close()
-        f.close()
+            func_dupl.append(func)
+
+    if not func_dupl:
+        print('Checked module {} not duplicate functions were found'.format(file))
+    else:
+        f = open(file, 'r')
+        f_name = file.split('.')
+        new_file_name = f_name[0] + '_cp.py'
+        f2 = open(new_file_name, 'w+')
+        i = 1
+        for line in f:
+            if 'test_' in line and 'x' not in line:
+                f2.write(line.replace(line[8:16], 'test_'+str(i).zfill(3)))
+                i += 1
+            else:
+                f2.write(line)
+        print('Checked module {} this functions were duplicates {} and fixed on {}'.format(file, func_dupl, cp))
+        f2.close()
+    f.close()
+    
