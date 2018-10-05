ff505b6dde865b0e34acb0966f0b8aace3162281
david <david.molina.py@gmail.com>
2018-10-05 09:54:44 -0700

    corrected f format

--- tests/number_verify.py
+++ tests/number_verify.py
@@ -6,13 +6,14 @@ Autor: David Molina @virtualdvid
 """
 number_verify: Checks each tests funtions names and re-enumerate if names are duplicated.
 """
+
 import glob
 
 test_files = [f for f in glob.glob("*_test.py")]
 
 for file in test_files:
     f = open(file, 'r')
-    func_list = [line[8:16] for line in f if line[8:13]=='test_']
+    func_list = [line[8:16] for line in f if line[8:13] == 'test_']
     func_unic = []
     func_dupl = []
     for func in func_list:
@@ -31,7 +32,7 @@ for file in test_files:
         i = 1
         for line in f:
             if 'test_' in line and 'x' not in line:
-                f2.write(line.replace(line[8:16], 'test_'+str(i).zfill(3)))
+                f2.write(line.replace(line[8:16], 'test_' + str(i).zfill(3)))
                 i += 1
             else:
                 f2.write(line)
